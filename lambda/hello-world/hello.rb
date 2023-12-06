2963d1a9340eef84719f2280230cfada7296d00a
Sarah Hernandez <sarah.hernandez@hashicorp.com>
2023-12-06 08:26:43 -0800

    Update hello.rb

--- lambda/hello-world/hello.rb
+++ lambda/hello-world/hello.rb
@@ -9,9 +9,31 @@ module LambdaFunctions
       {
         statusCode: 200,
         headers: {
-          'Content-Type' => 'text/plain'
+          'Content-Type' => 'text/html; charset=UTF-8'
         },
-        body: "Hello, #{name}!",
+        body: "<html>
+                <body>
+                  <h1>Hello, #{name}!</h1>
+                  <main>
+                    <section>
+                      <h2>Cat Photos</h2>
+                      <!-- TODO: Add link to cat photos -->
+                      <p>Click here to view more <a target="_blank" href="https://freecatphotoapp.com">cat photos</a>.</p>
+                      <a href="https://freecatphotoapp.com"><img src="https://cdn.freecodecamp.org/curriculum/cat-photo-app/relaxing-cat.jpg" alt="A cute orange cat lying on its back."></a>
+                    </section>
+                    <section>
+                      <h2>Cat Lists</h2>
+                      <h3>Things cats love:</h3>
+                      <ul>
+                        <li>cat nip</li>
+                        <li>laser pointers</li>
+                        <li>lasagna</li>
+                      </ul>
+                    </section>
+                  </main>
+                </body>
+              </html>
+          ",
       }
     end
   end
