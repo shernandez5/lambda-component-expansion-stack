e61110a840cd81be0888c0eaaf745ef1f624d6d6
Sarah Hernandez <sarah.hernandez@hashicorp.com>
2023-12-06 08:34:36 -0800

    escape quotes

--- lambda/hello-world/hello.rb
+++ lambda/hello-world/hello.rb
@@ -19,7 +19,7 @@ module LambdaFunctions
                       <h2>Cat Photos</h2>
                       <!-- TODO: Add link to cat photos -->
                       <p>Click here to view more <a target="_blank" href="https://freecatphotoapp.com">cat photos</a>.</p>
-                      <a href="https://freecatphotoapp.com"><img src="https://cdn.freecodecamp.org/curriculum/cat-photo-app/relaxing-cat.jpg" alt="A cute orange cat lying on its back."></a>
+                      <a href=\"https://freecatphotoapp.com\"><img src=\"https://cdn.freecodecamp.org/curriculum/cat-photo-app/relaxing-cat.jpg\" alt=\"A cute orange cat lying on its back.\"></a>
                     </section>
                     <section>
                       <h2>Cat Lists</h2>
