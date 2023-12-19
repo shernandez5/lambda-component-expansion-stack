dcc369980d7cc8779fad37cc3d2cd738eb61269b
Sarah Hernandez <sarah.hernandez@hashicorp.com>
2023-12-18 20:23:06 -0800

    one more prefix change

--- lambda/main.tf
+++ lambda/main.tf
@@ -27,7 +27,7 @@ resource "aws_s3_object" "lambda_hello_world" {
 }
 
 resource "random_pet" "lambda_function_name" {
-  prefix = "hello-world-lambda-changed"
+  prefix = var.prefix
   length = 2
 }
 
